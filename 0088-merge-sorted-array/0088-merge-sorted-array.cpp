56f9d9895a814dba48498599f4299bc6a52e626c
Rahul Kumar <128255986+rahulkr-24@users.noreply.github.com>
2023-09-06 17:20:14 +0530

    Time: 6 ms (6.92%), Space: 9.1 MB (46.26%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -7,26 +7,20 @@ public:
 //         }
 //         sort(nums1.begin(),nums1.end());
 //        }
-        vector<int> result;
         int index1=0,index2=0;
         while(index1 < m && index2 < n)
         {
-            if(nums1[index1]<nums2[index2])
-                result.push_back(nums1[index1++]);
+            if(nums1[index1]<=nums2[index2])
+               index1++;
             else
-                result.push_back(nums2[index2++]);
-        }
-        while(index1<m)
-        {
-            result.push_back(nums1[index1++]);
+            {
+                swap(nums1[index1++],nums2[index2]);
+                sort(nums2.begin(),nums2.end());
+            }
         }
         while(index2<n)
         {
-            result.push_back(nums2[index2++]);
-        }
-        for(int i=0;i<result.size();i++)
-        {
-            nums1[i]=result[i];
+            nums1[index1++]=nums2[index2++];
         }
     }
     
