69cf51bc0de7843b4bcf22d74c6655c50180e5c5
Rahul Kumar <128255986+rahulkr-24@users.noreply.github.com>
2024-01-31 00:44:27 +0530

    Time: 15 ms (61.22%), Space: 20.4 MB (8.60%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,12 +3,11 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
         long actualSum = (n*(n+1))/2;
-        long sum = 0;
         for(int element: nums)
         {
             actualSum -= element;
         }
 
-        return abs(actualSum);
+        return actualSum;
     }
 };
